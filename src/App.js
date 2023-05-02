7b7dad3370369479c1e5df2eb4c84d517023f78c
Syyd007 <sayedsoudana054@gmail.com>
2023-05-02 10:13:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  // eslint-disable-next-line react/require-render-return
+  state = {
+    fullName: "Sayed soudana",
+    bio: "full stack js developer ",
+    imgSrc: "vvvv",
+    profession: "student ",
+    timer:0,
+    display: true
+  }
+  disbutton = () => {
+    this.setState({ display: !this.state.display })
+  }
+  componentDidMount() {
+    console.log("Counter Mounted");
+    setInterval(() => {this.setState({timer:this.state.timer+1})}, 1000);
+}
+  render() {
+
+    return (
+      <div className="App">
+        <div>
+          <button onClick={this.disbutton}>{this.state.display ? "Hide" : "Show"}</button>
+          {this.state.display ?
+            <div>
+              <span >profile </span>
+              
+              <span className='block'>fullName: {this.state.fullName} </span>
+              <span className='block'> bio : {this.state.bio}</span>
+              <span className='block'>photo :{this.state.imgSrc} </span>
+              <span className='block'>profession: {this.state.profession} </span>
+              <h4>Counter Timer:{this.state.timer}</h4>
+            </div>
+            : null}
+        </div>
+        </div>
+        )
+  
+  }
+}
+
+
+
+        export default App;
